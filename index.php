60556ac3f6218a517b5e6975fd7f3c5442efbbf5
yeison <yschamorro@unicauca.edu.co>
2022-10-31 22:58:43 -0500

    para php

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Domotic System</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<header>
+    <a href="#" class="logo">
+        <span>Sistema domotico</span></a>
+
+        <input type="checkbox" id="menu-bar">
+        <label for="menu-bar" class="fa fa-bars"></label>
+        <nav class="navbar">
+            <a href="#inicio">inicio</a>
+            <a href="#modo">habitaciones</a>
+            <a href="#login">login</a>
+        </nav>
+</header>
+<body>
+    <section class="inicio" id="inicio">
+        <div class="content">
+            <h3>ILUMINA<span> TU HOGAR</span></h3>
+           <!-- <a href="#" class="btn">Cambiar habitacion</a>-->
+           <ul class="navegacion">
+                    <li><a href="#">Modo de ilimunacion</a>
+                    <ul>     
+                    <li><a href="#">Modo remoto</a>
+                          <?php
+                          añ
+                           ?>       
+                    
+                        <li><a href="#">Modo Automatico</a>
+                        <li><a href="#">modo Sensado</a>
+                    </ul>
+                    </li>
+                    <li><a href="#">Encender led</a>
+                       <ul>
+                           <li><a href="#">Habitacion 1</a>
+                                <ul>
+                                    <li><a1 href="#">Encender</a1></li>
+                                    <li><a1 href="#">Apagar</a1></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Habitacion 2</a>
+                               <ul>
+                                    <li><a1 href="#">Encender</a1></li>
+                                    <li><a1 href="#">Apagar</a1></li>
+                                </ul>
+                            </li>
+                           <li><a href="#">Habitacion 3</a>
+                                <ul>
+                                    <li><a1 href="#">Encender</a1></li>
+                                    <li><a1 href="#">Apagar</a1></li>
+                            </ul>
+                        </li>
+                       </ul>
+                    </li>
+                    </li>
+                    
+                    <li><a href="#">Estado de habitaciones</a>
+                    <ul>     
+                    <li><a href="#">Modo remoto</a>
+                        <li><a href="#">Modo Automatico</a>
+                        <li><a href="#">modo Sensado</a>
+                    </ul>
+                    </li>
+                         <li><a href="#"> Modo de Iluminacion Actual</a>
+                        <ul>     
+                            <li><a href="#"> modo de Iluminacion</a>
+                        </ul>
+                 
+               </ul>
+           </div>
+           
+         </div>
+        
+        <div class="img">
+            <img src="img/ " width="" height="" alt="">
+        </div>
+    </section>
+</body>
+</html>
