c4c8e4c439a8c237d132285e5adc140f0c8e7d6d
unknown <vidahoro@gmail.com>
2022-11-03 11:34:27 -0500

    espacio

--- serverConections/connetion.js
+++ serverConections/connetion.js
@@ -3,7 +3,7 @@ var conexion= connection.createConnection({
     host : 'localhost',
     user : 'u628361878_lab3',
     password : 'Domotica#3245',
-    database : 'u628361878_laboratorio3	'
+    database : 'u628361878_laboratorio3'
 
 });
 
