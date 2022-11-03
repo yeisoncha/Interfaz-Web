a448e7d27faffa424d5e5d17c00c14dd05f6aaef
unknown <vidahoro@gmail.com>
2022-11-03 16:19:57 -0500

    modificación servidores

--- serverConections/connetion.js
+++ serverConections/connetion.js
@@ -1,32 +1,48 @@
-var connection = require('mysql');
-var conexion= connection.createConnection({
+var mysql = require('mysql');
+var conexion= mysql.createConnection({
     host : 'localhost',
-    user : 'u628361878_lab3',
-    password : 'Domotica#3245',
-    database : 'u628361878_laboratorio3'
+    user : 'root',
+    password : '',
+    database : 'prueba'
 
 });
+//Domotica#3245
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }
+    else{
+        console.log("Conexion exitosa")
+    }
 
-const conectar  =() =>{
-    conexion.connect(err => {
-        if(err) throw  err 
-        console.log("Conectado a la DB");
-    })
+})
 
-}
+// conexion.end();
+
+// const conectar  =() =>{
+//     conexion.connect(err => {
+//         if(err) throw  err 
+//         console.log("Conectado a la DB");
+//     })
+
+// }
 
 
-const consultarEstado=(id,idtarget,hab1,hab2,hab3,modo) =>{
-   conectar();
-    let  query = "INSERT INTO estados  VALUES(NULL,"+idtarget+","+hab1+","+hab2+","+hab3+",)"
-    conexion.query(query,function(err,result,filed) {
-        if (err) throw err
-        console.log(result);
 
-    })
-}
 
-module.exports ={
-    conectar
 
-}
+// const consultarEstado=(ID_TARJ,hab1,hab2,hab3,modo) =>{
+//     //conectar();
+//     let  query = "INSERT INTO estados (ID_TARJ,hab1,hab2,hab3,modo)" + "VALUES('"+1+"','"+1+"','"+1+"','"+1+",'"+1+"')"
+//     conexion.query(query,function(err,result,filed) {
+//         if (err) throw err
+//         console.log(result);
+
+//     })
+// }
+
+// module.exports ={
+//     conectar,
+//     consultarEstado
+
+// }
