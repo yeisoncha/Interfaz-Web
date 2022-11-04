19a26ecefbe9cc01fa8350f501cbe3db2671c8b6
jefferson <jeffersonsteven231997@gmail.com>
2022-11-04 02:23:45 -0500

    req luces apagadas

--- index.php
+++ index.php
@@ -32,13 +32,12 @@
             <h3>ILUMINA<span> TU HOGAR</span></h3>
            <!-- <a href="#" class="btn">Cambiar habitacion</a>-->
            <ul class="navegacion">
-                    <li><a href="#" >Modos de ilimunación</a>
+                    <li><a href="#" >Modos de Iluminacion</a>
                     <ul>     
-                    <li><a href="#" class="btn">Modo remoto</a>
-                             
-                    
-                        <li><a href="#" class="btn">Modo automatico</a>
-                        <li><a href="#" class="btn">modo sensado</a>
+                        
+                        <button class="btn" type="submit" name = "MODOREMOTO" >Modo Remoto</button>
+                        <button class="btn" type="submit" name = "MODOAUTOMATICO" >Modo Automatico</button>
+                        <button class="btn" type="submit" name = "MODOSENSADO" >Modo Sensado</button>
                     </ul>
                     </li>
                     <li><a href="#">Encender bombilla</a>
@@ -67,7 +66,7 @@
                     
                     <li><a href="#">Estado de iluminación</a>
                     <ul>     
-                    <li><a href="" class="btn">Habitación 1: Encendido </a>
+                        <li><a href="#" class="btn" type="submit" name = "ON3" >ON</button>
                         <li><a href="#" class="btn">Habitación 2:</a>
                         <li><a href="#" class="btn">Habitación 3:</a>
                     </ul>
@@ -93,7 +92,7 @@
     $mysqli = new mysqli($host, $user, $pw, $db);
     if(isset($_POST['ON1']))			// If press ON
     {	
-        echo("hoooola");       
+              
         $sql = "UPDATE estados set hab1='1' where ID_TARJ='1'";	// Update present status to database
     //             // If don't put this If , we can't change the value in database
         if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
@@ -102,7 +101,7 @@
     }
     if(isset($_POST['OFF1']))			// If press ON
     {	
-        echo("hoooola");       
+              
         $sql = "UPDATE estados set hab1='0' where ID_TARJ='1'";	// Update present status to database
     //             // If don't put this If , we can't change the value in database
         if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
@@ -112,7 +111,6 @@
 
     if(isset($_POST['ON2']))			// If press ON
     {	
-        echo("hoooola");       
         $sql = "UPDATE estados set hab2='1' where ID_TARJ='1'";	// Update present status to database
     //             // If don't put this If , we can't change the value in database
         if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
@@ -121,7 +119,6 @@
     }
     if(isset($_POST['OFF2']))			// If press ON
     {	
-        echo("hoooola");       
         $sql = "UPDATE estados set hab2='0' where ID_TARJ='1'";	// Update present status to database
     //             // If don't put this If , we can't change the value in database
         if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
@@ -130,7 +127,6 @@
     }
     if(isset($_POST['ON3']))			// If press ON
     {	
-        echo("hoooola");       
         $sql = "UPDATE estados set hab3='1' where ID_TARJ='1'";	// Update present status to database
     //             // If don't put this If , we can't change the value in database
         if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
@@ -139,11 +135,63 @@
     }
     if(isset($_POST['OFF3']))			// If press ON
     {	
-        echo("hoooola");       
         $sql = "UPDATE estados set hab3='0' where ID_TARJ='1'";	// Update present status to database
     //             // If don't put this If , we can't change the value in database
         if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
         //                 $_GET[] = 1;						// why i have to put this line but it still run when it's commented
         } 
+    }
+    if(isset($_POST['MODOREMOTO']))			// If press ON
+    {	
+        $sql2 = "SELECT hab1,hab2,hab3 from estados where ID_TARJ=1";
+        $result = $mysqli->query($sql2);
+        $row = $result->fetch_array(MYSQLI_NUM);
+        $hab1 = $row[0]; 
+        $hab2 = $row[1];
+        $hab3 = $row[2];
+        if($hab1 == 0 and $hab2 ==0 and $hab3 == 0){
+            $sql = "UPDATE estados set modo='0' where ID_TARJ='1'";	// Update present status to database
+    //             // If don't put this If , we can't change the value in database
+            if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
+            //                 $_GET[] = 1;						// why i have to put this line but it still run when it's commented
+            }
+        }
+
+         
+    }
+    if(isset($_POST['MODOAUTOMATICO']))			// If press ON
+    {	
+
+        $sql2 = "SELECT hab1,hab2,hab3 from estados where ID_TARJ=1";
+        $result = $mysqli->query($sql2);
+        $row = $result->fetch_array(MYSQLI_NUM);
+        $hab1 = $row[0]; 
+        $hab2 = $row[1];
+        $hab3 = $row[2];
+        if($hab1 == 0 and $hab2 ==0 and $hab3 == 0){
+            $sql = "UPDATE estados set modo='1' where ID_TARJ='1'";	// Update present status to database
+    //             // If don't put this If , we can't change the value in database
+            if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
+            //                 $_GET[] = 1;						// why i have to put this line but it still run when it's commented
+            } 
+        }
+        
+    }
+    if(isset($_POST['MODOSENSADO']))			// If press ON
+    {	
+        $sql2 = "SELECT hab1,hab2,hab3 from estados where ID_TARJ=1";
+        $result = $mysqli->query($sql2);
+        $row = $result->fetch_array(MYSQLI_NUM);
+        $hab1 = $row[0]; 
+        $hab2 = $row[1];
+        $hab3 = $row[2];
+        if($hab1 == 0 and $hab2 ==0 and $hab3 == 0){
+            $sql = "UPDATE estados set modo=2 where ID_TARJ='1'";	// Update present status to database
+    //             // If don't put this If , we can't change the value in database
+            if ($mysqli->query($sql) === TRUE) {	// Because it's been a long time , so i forgot
+            //                 $_GET[] = 1;						// why i have to put this line but it still run when it's commented
+            } 
+        }
+        
     }
 ?>
