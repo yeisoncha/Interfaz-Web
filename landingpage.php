2bb66f3d5d0ef72156e5f8ca4f9db4b568d1219f
vidahoro2 <vidahoro@gmail.com>
2022-11-18 10:52:41 -0500

    primeros archivos para inicio de sesión

---
+++ landingpage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="validar.php">
+    <div class="container-iniciar-sesion">
+        <label for="Name">Usuario: </label>
+        <input type="text" name="" id="Name">
+        <label for="password">Contraseña</label>
+        <input type="password" id="password">
+        <button type="submit">Iniciar Sesión</button>
+
+
+    </div>
+    </form>
+    
+    
+</body>
+</html>
