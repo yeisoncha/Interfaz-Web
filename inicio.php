42f7add1724501bec26f1ca6be5d2ba9264529ab
vidahoro2 <vidahoro@gmail.com>
2022-12-08 21:32:01 -0500

    error mostrar viviendas done

--- inicio.php
+++ inicio.php
@@ -35,9 +35,9 @@
                 </tr>
                 <?php
                     $cnx = mysqli_connect("localhost","root","","newiot");
-                    $sql = "SELECT id, direccion, habitacion, house_code from viviendas order by id desc";
+                    $sql = "SELECT id, dirección, habitaciones, house_code from viviendas order by id desc";
                     $rta = mysqli_query($cnx,$sql);
-                    while ($mostrar == mysqli_fetch_row($rta)){
+                    while ($mostrar = mysqli_fetch_row($rta)){
                     ?>
                     <tr>
                         <td><?php echo $mostrar['0']?></td>
