c7d2534d9e7cceb16bbb635186753a98249d7845
vidahoro2 <vidahoro@gmail.com>
2022-12-08 21:17:37 -0500

    error crear viviendas

--- inicio.php
+++ inicio.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>Domotic System</title>
-    <link rel="stylesheet"  href="/css/inicio.css">
+    <link rel="stylesheet"  href="css/inicio.css">
 </head>
 <header>
     <a href="#" class="logo">
@@ -34,10 +34,10 @@
                     <td>Opciones</td>
                 </tr>
                 <?php
-                    $cnx = mysqli_connect("localhost","root","","nase");
+                    $cnx = mysqli_connect("localhost","root","","newiot");
                     $sql = "SELECT id, direccion, habitacion, house_code from viviendas order by id desc";
                     $rta = mysqli_query($cnx,$sql);
-                    while ($mostrar = mysqli_fetch_row($rta)){
+                    while ($mostrar == mysqli_fetch_row($rta)){
                     ?>
                     <tr>
                         <td><?php echo $mostrar['0']?></td>
